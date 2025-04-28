b9b5062cbe1bc10589d761c6735d3a0d45b049a6
antoniocodemaster <antoniocodemaster@gmail.com>
2025-04-28 08:32:58 -0600

    Adding devtools to useAuthState

--- src/lib/states/AuthState.ts
+++ src/lib/states/AuthState.ts
@@ -1,4 +1,5 @@
 import { create } from 'zustand';
+import { devtools } from 'zustand/middleware';
 import { loginWithCredentialsApi } from '../../api/AuthApi';
 
 interface AuthState {
@@ -8,24 +9,30 @@ interface AuthState {
   logout: () => void;
 }
 
-const useAuthState = create<AuthState>((set, get) => ({
-  token: localStorage.getItem('token') || null,
-  userLoggedIn: () => !!get().token,
-  loginWithCredentials: async (email: string, password: string) => {
-    try {
-      const response = await loginWithCredentialsApi(email, password);
-      localStorage.setItem('token', response.token);
-      set({ token: response.token });
-      return response;
-    } catch (error) {
-      console.error('Error logging in', error);
-      throw error;
-    }
-  },
-  logout: () => {
-    localStorage.removeItem('token');
-    set({ token: null });
-  },
-}));
+const useAuthState = create<AuthState>()(
+  devtools(
+    (set, get) => ({
+      token: localStorage.getItem('token') || null,
+      something: 'something',
+      userLoggedIn: () => !!get().token,
+      loginWithCredentials: async (email: string, password: string) => {
+        try {
+          const response = await loginWithCredentialsApi(email, password);
+          localStorage.setItem('token', response.token);
+          set({ token: response.token });
+          return response;
+        } catch (error) {
+          console.error('Error logging in', error);
+          throw error;
+        }
+      },
+      logout: () => {
+        localStorage.removeItem('token');
+        set({ token: null });
+      },
+    }),
+    { name: 'AuthState' }
+  )
+);
 
 export default useAuthState;
